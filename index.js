11e4b17e8da72477a5295bdd78c671096c6d38da
mukitur <mukit.inway@gmail.com>
2023-10-16 21:48:15 +0600

    post operation done

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db('usersDB').collection('users');
+
+    // add users to DB
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
@@ -33,7 +45,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
